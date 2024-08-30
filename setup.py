f962efbb43fa694e2a1f5aca6d2af25869983199
Denis <dngaspar5@gmail.com>
2024-08-30 01:03:47 +0000

    Release v2024.08.30.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bitgate",
-    version="v2024.08.30.5",
+    version="v2024.08.30.6",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
